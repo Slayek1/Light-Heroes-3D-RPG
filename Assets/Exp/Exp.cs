201bd87ee20cccb1b6dacf07af9dcce3f78d65ef
Slayek1 <32570471+Slayek1@users.noreply.github.com>
2021-04-21 21:17:53 +0200

    a

---
+++ Assets/Exp/Exp.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exp : MonoBehaviour {
+
+	public static int currentExp;
+	public int internalExp;
+	// Update is called once per frame
+	void Update () {
+		internalExp = currentExp;
+	}
+}
---
+++ Assets/Exp/GlobalLevel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalLevel : MonoBehaviour {
+
+	public static int currentLevel=1;
+	public int internalLevel;
+	// Update is called once per frame
+	void Update () {
+		internalLevel = currentLevel;
+	}
+}
